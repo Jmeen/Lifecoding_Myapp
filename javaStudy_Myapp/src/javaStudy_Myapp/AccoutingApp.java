e0a9cf1bb8e6ca24ede257b7518ce7553754c7bf
Jaemin Lim <jmeeen@gmail.com>
2021-07-18 16:30:18 +0900

    메서드 생성

--- javaStudy_Myapp/src/javaStudy_Myapp/AccoutingApp.java
+++ javaStudy_Myapp/src/javaStudy_Myapp/AccoutingApp.java
@@ -1,8 +1,11 @@
 package javaStudy_Myapp;
 
 public class AccoutingApp {
-
-	public static void main(String[] args) {
+	// 전역변수
+	public static double valueOfSupply;
+	public static double vatRate;
+	public static double expanseRate;
+	public static void main(String[] args ) {
 		// 생활코딩 14.2 나의 앱 만들기 1
 		// 변수로 변경
 		// 입력값 도입
@@ -10,18 +13,18 @@ public class AccoutingApp {
 		// 제어문 추가
 		// 배열
 		// 반복문 추가
-		double valueOfSupply = Double.parseDouble(args[0]);
-		double vatRate = 0.1;
-		double VAT = valueOfSupply * vatRate;
-		double Total = valueOfSupply + VAT;
-		double expanseRate = 0.3;
-		double Expense = valueOfSupply * expanseRate;
-		double income = valueOfSupply - Expense;
+		// 메소드 추가 -> 메소드서로 연관된 코드를 그룹핑하여 이름을 붙인 정리정돈의 상자
+		valueOfSupply = 10000;
+		vatRate = 0.1;
+		expanseRate = 0.3;
+		double VAT = getVAT();
+		double Total = getTotal();
+		double Expense = getExpense();
+		double income = getIncome();
 		
-		double rate1 = 0.5;
-		double rate2 = 0.3;
-		double rate3 = 0.2;
-		
+//		double rate1 = 0.5;
+//		double rate2 = 0.3;
+//		double rate3 = 0.2;	
 		//Rate를 배열로 생성
 //		if (income > 10000.0) {
 //			dividend1 = income * 0.5;
@@ -32,12 +35,8 @@ public class AccoutingApp {
 //			dividend2 = income * 0;
 //			dividend3 = income * 0;
 //		}
-
-		System.out.println("VAT : " + VAT);
-		System.out.println("Value of supply : " + valueOfSupply);
-		System.out.println("Total : " + Total);
-		System.out.println("Expense : " + Expense);
-		System.out.println("income : " + income);
+		
+		print();
 		
 		// 이 아래 3줄은 같은 일을 하고있다. 반복문으로 바꿔보자.
 		
@@ -55,6 +54,30 @@ public class AccoutingApp {
 			System.out.println("Dividend1 : " + income*dividendRates[i]);
 			i = i + 1;
 		}
+	}
+
+	private static void print() {
+		System.out.println("VAT : " + getVAT());
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("Total : " + getTotal());
+		System.out.println("Expense : " + getExpense());
+		System.out.println("income : " + getIncome());
+	}
+
+	private static double getIncome() {
+		return valueOfSupply - getExpense();
+	}
+
+	private static double getExpense() {
+		return valueOfSupply * expanseRate;
+	}
+
+	private static double getTotal() { 
+		return valueOfSupply + getVAT();
+	}
+
+	private static double getVAT() {
+		return valueOfSupply * vatRate;
 	}
 
 }
